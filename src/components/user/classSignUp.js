92bf712dd9767e6c417cbf0c55d8076f32f5c0af
Athanasios Chatzigiannakos <62661213+thanasischatzigiannakos@users.noreply.github.com>
2021-08-22 18:08:55 +0300

    Completed class registration(and unregister).

--- src/components/user/classSignUp.js
+++ src/components/user/classSignUp.js
@@ -9,6 +9,7 @@ import Form from 'react-bootstrap/Form';
 import { useHistory } from 'react-router-dom';
 import axiosInstance from '../../axios';
 import React, { useState, useEffect, useRef } from 'react';
+import "./classSignUp.css";
 
 
 export default function ClassSignUp() {
@@ -17,6 +18,8 @@ export default function ClassSignUp() {
     const [loading, setLoading] = useState(false)
     const [userClasses, classesUpdate] = useState([]);
     const [listTeachings, teachingsUpdate] = useState([]);
+    const [teachingId, updateTeachingID] = useState(null)
+    const [userInfo, infoUpdate] = useState([]);
 
     async function Classes() {
         console.log('Fetching...')
@@ -30,7 +33,7 @@ export default function ClassSignUp() {
 
     }
 
-    async function AlreadySignedUp(){
+    async function AlreadySignedUp() {
         console.log('Fetching...')
         try {
             axiosInstance.get('classes/SignedUp/' + localStorage.getItem("user_id")).then((res) => {
@@ -43,6 +46,38 @@ export default function ClassSignUp() {
 
     }
 
+    const handleRegister = (e) => {
+        try {
+            axiosInstance
+                .post(`classes/createSignUp/`, {
+                    teaching: {
+                        id: e.target.value
+                    },
+                    student: {
+                        user: localStorage.getItem("user_id")
+                    },
+                })
+                .then((res) => {
+                    console.log(res)
+                    AlreadySignedUp()
+
+                });
+        } catch (error) { console.log(error.message) }
+
+    };
+
+    const handleUnregister = (e) => {
+        console.log(e.target.value)
+        const thisTeaching= userClasses.find(object => {return object.teaching.id = e.target.value})
+        console.log(thisTeaching.id)
+        try {
+            axiosInstance.delete('classes/deleteSignUp/' + thisTeaching.id).then((res) => {
+                AlreadySignedUp()
+            });
+        } catch (error) { console.log(error.message) }
+
+    };
+
 
 
 
@@ -51,17 +86,38 @@ export default function ClassSignUp() {
         AlreadySignedUp();
 
 
-    }, [listTeachings.join(',')],[userClasses.join(',')])
-    
+    }, [listTeachings.join(',')], [userClasses.join(',')])
+
     return (
         <div>
-        <NavBar></NavBar>
-         <h2>Sign Up to Available Classes</h2>
-         <div>
-             {listTeachings.map(teaching => (
-                 <li key={teaching.id}>{Object.values(teaching.schoolClass.name).join(' ')}</li>
-             ))}
-         </div>
+            <NavBar></NavBar>
+            <h2>Sign Up to Available Classes</h2>
+            <div className="classesListed">
+                <ListGroup >
+                    {listTeachings.map(teaching => (
+                        <ListGroupItemfrom key={teaching.id}>
+                            <div className="listItem">
+                                <div className="classInfo">
+                                    <b>{Object.values(teaching.schoolClass.name)}</b>
+                                    <p>Professor: {Object.values(teaching.professor.user.first_name)} {Object.values(teaching.professor.user.last_name)}.</p>
+                                    <p>{Object.values(teaching.schoolClass.description)} .The final grade for the class is calculated as follows :
+                                        {teaching.theory_weight * 100}% from the theory score and {teaching.lab_weight * 100}% from the lab score.</p>
+
+                                </div>
+                                <div className="subToClass">
+                                    {userClasses.filter(e => e.teaching.schoolClass.id === teaching.schoolClass.id).length === 0 ? 
+                                    <Button value={teaching.id} className="w-100" type="submit" variant="dark" style={{ width: '100%', height: '75%' }}
+                                        onClick={handleRegister}>Register to Class</Button>
+                                        : <Button value={teaching.id} 
+                                        className="w-100" type="submit" variant="dark" style={{ width: '100%', height: '75%' }} onClick={handleUnregister}>Unregister</Button>
+                                    }
+                                </div>
+                            </div>
+                        </ListGroupItemfrom>
+                    ))}
+                </ListGroup>
+
+            </div>
 
         </div>
     )
